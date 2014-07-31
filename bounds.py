8c7a254feff2aa5d953854f532d0c5da3619bec6
mlhales <mike@muse-software.com>
2014-07-30 21:44:30 -0700

    Added Bounds class. It updates with the movement of the bug, and will let us calculate the bounds where the bug will bounce.

---
+++ bounds.py
@@ -0,0 +1,45 @@
+__author__ = 'mike'
+
+import cv2
+
+
+class Bounds():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.w = 0
+        self.h = 0
+
+    def expand_box(self, box):
+        for point in box:
+            self.expand_point(point[0], point[1])
+
+    def expand_point(self, x, y):
+        if x < self.x:
+            self.w += self.x - x
+            self.x = x
+        if y < self.y:
+            self.h += self.y - y
+            self.y = y
+        if x > (self.x + self.w):
+            self.w = x - self.x
+        if y > (self.y + self.h):
+            self.h = y - self.y
+
+    def exceeds(self, x, y):
+        response = False
+        xi = x
+        yi = y
+        if x < self.x:
+            xi = self.x
+            response = True
+        if y < self.y:
+            yi = self.y
+            response = True
+        if x > self.x + self.w:
+            xi = self.x + self.w
+            response = True
+        if y > self.y + self.h:
+            yi = self.y + self.h
+            response = True
+        return response, xi, yi
--- hexbug_tracker.py
+++ hexbug_tracker.py
@@ -2,18 +2,20 @@ import sys
 import getopt
 import cv2
 import numpy as np
+import bounds
 
 
 def nothing(x):
     pass
 
 
-def track_hexbug():
-    cap = cv2.VideoCapture('Video/hexbug-training_video.mp4')
+def track_hexbug(source = 'Video/hexbug-training_video.mp4'):
+    cap = cv2.VideoCapture(source)
     fout = open('centroid_data', 'w')
     fout.write('[')
 
     centroid = []
+    bnd = None
     ctr = 0
 
     hmin = (238 - 30) / 2
@@ -45,7 +47,7 @@ def track_hexbug():
         except:
             break
 
-            # #    # create trackbars for color change
+            ##    # create trackbars for color change
             ##    cv2.createTrackbar('H','mask',0,180,nothing)
             ##    cv2.createTrackbar('S','mask',0,255,nothing)
             ##    cv2.createTrackbar('V','mask',0,255,nothing)
@@ -75,14 +77,26 @@ def track_hexbug():
         contours, hierarchy = cv2.findContours(thresh, 1, 2)
         if contours:
             cnt = contours[0]
+            rect = cv2.minAreaRect(cnt)
+            box = cv2.cv.BoxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(frame, [box], 0, (0, 0, 255), 1)
             M = cv2.moments(cnt)
             if M['m00'] != 0:
                 cx = int(M['m10'] / M['m00'])
                 cy = int(M['m01'] / M['m00'])
                 centroid.append([cx, cy])
+                cv2.circle(frame, (cx, cy), 2, (255, 0, 0))
+                if bnd:
+                    bnd.expand_box(box)
+                else:
+                    bnd = bounds.Bounds(cx, cy)
             else:
                 centroid.append([-1, -1])
-
+        if bnd:
+            cv2.rectangle(frame, (bnd.x, bnd.y), (bnd.x + bnd.w, bnd.y + bnd.h), (255, 255, 0), 2)
+        cv2.imshow("Capture", frame)
+        cv2.waitKey(1)
     # #    k = cv2.waitKey(5) & 0xFF
     ##    if k == 27:
     ##        break
@@ -116,7 +130,10 @@ def main(argv=None):
             opts, args = getopt.getopt(argv[1:], "h", ["help"])
         except getopt.error, msg:
             raise Usage(msg)
-        track_hexbug()
+        if len(args):
+            track_hexbug(args[0])
+        else:
+            track_hexbug()
     except Usage, err:
         print >> sys.stderr, err.msg
         print >> sys.stderr, "for help use --help"
